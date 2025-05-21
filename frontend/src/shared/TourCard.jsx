72de22a8db54b57fbc421baef55259283428a20b
desipasilpiaarum <emailkamu@gmail.com>
2025-05-21 20:41:43 +0700

    change/tourcard

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -2,49 +2,47 @@ import React from "react";
 import { Card, CardBody } from "reactstrap";
 import { Link } from 'react-router-dom';
 import calculateAvgRating from "../utils/avgRating";
-
 import './tour-card.css';
 
 const TourCard = ({ tour }) => {
+  const { id, title, city, photo, price, featured, reviews } = tour;
+  const { totalRating, avgRating } = calculateAvgRating(reviews);
 
-    const { id, title, city, photo, price, featured, reviews } = tour
-    
-const {totalRating, avgRating} = calculateAvgRating(reviews)
+  return (
+    <div className="tour__card">
+      <Card>
+        <div className="tour__img">
+          <img src={photo} alt="tour-img" />
+        </div>
 
-    return <div className="tour__card">
-        <Card>
-            <div className="tour__img">
-                <img src={photo} alt="tour-img" />
-                
-            </div>
+        <CardBody>
+          <div className="card__top d-flex align-items-center justify-content-between">
+            <span className="tour__location d-flex align-items-center gap-1">
+              <i className="ri-map-pin-line"></i> {city}
+            </span>
 
-            <CardBody>
-                <div className="card__top d-flex align-items-center justify-content-between">
-                    <span className="tour__location d-flex align-items-center gap-1">
-                        <i class="ri-map-pin-line"></i>{city}
-                    </span>
-                    <span className="tour__rating d-flex align-items-center gap-1">
-                        <i class="ri-star-fill"></i> {avgRating === 0 ? null :avgRating } 
-                        {totalRating === 0 ? ("Nt Rated" ):(
-                        <span>({reviews.length})</span>
-                        )}
-                    </span>
+            <span className="tour__rating d-flex align-items-center gap-1">
+              <i className="ri-star-fill"></i> 
+              {avgRating === 0 ? null : avgRating}
+              {totalRating === 0 ? (
+                "Not Rated"
+              ) : (
+                <span> ({reviews.length})</span>
+              )}
+            </span>
+          </div>
 
-                </div>
-                <h5 className="tour__title"><Link to={'/tours/${id}'}>{title}</Link></h5>
+          <h5 className="tour__title">
+            <Link to={`/tour/${id}`}>{title}</Link>
+          </h5>
 
-               
-
-                    <button className="btn booking__btn">
-                        <Link to={'/tours/${id}'}>Kunjungi Sekarang</Link>
-                    </button>
-               
-            </CardBody>
-        </Card>
-
-
-    </div>;
-
+          <button className="btn booking__btn">
+            <Link to={`/tour/${id}`}>Kunjungi Sekarang</Link>
+          </button>
+        </CardBody>
+      </Card>
+    </div>
+  );
 };
 
 export default TourCard;
